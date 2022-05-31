015e5812bf09399b5b6c0ff37d3f18c8be8c3894
Shaun Middlebusher <shaun.m@planetscale.com>
2022-05-31 12:48:36 -0700

    Noble UI kit push
    ui-kit push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Prosimo';
+export class AppComponent implements OnInit {
+  title = 'nobleui-angular';
+
+  ngOnInit(): void {}
+
 }
